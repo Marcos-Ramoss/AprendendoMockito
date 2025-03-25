aae2006c3a75fdf5e7e24efeea96753a617f190d
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-03-25 15:20:01 -0400

    Fazendo o primeiro mock com mockito

---
+++ src/test/java/br/com/marcos/cursos_business/CourseBusinessMockTest.java
@@ -0,0 +1,34 @@
+package br.com.marcos.cursos_business;
+
+import br.com.marcos.service.CourseService;
+import br.com.marcos.service.stubs.CourseServiceStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+
+public class CourseBusinessMockTest {
+
+    CourseService mockService;
+    CourseBusiness business;
+
+    @BeforeEach
+    void stup(){
+        mockService = mock(CourseService.class);
+        business = new CourseBusiness(mockService);
+    }
+
+    @Test
+    void testCoursesRelatedToSpring_When_UsingMock(){
+
+        var filtrarCurso = business.recuperarCurso("Leandro");
+
+        assertEquals(4, filtrarCurso.size());
+
+    }
+
+
+
+}
--- src/test/java/br/com/marcos/cursos_business/CourseBusinessStubTest.java
+++ src/test/java/br/com/marcos/cursos_business/CourseBusinessStubTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 
-public class CourseBusinessTest {
+public class CourseBusinessStubTest {
 
     @Test
     void testCoursesRelatedToSpring_When_UsingAstub(){
